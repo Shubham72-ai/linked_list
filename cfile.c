0c99bba738c6c9b2bdc6e8b10e851ccc06680459
Shubham Das <shubhamdasblgt@gmail.com>
2019-11-21 20:37:07 +0530

    this is insert thing

--- cfile.c
+++ cfile.c
@@ -3,54 +3,66 @@
 
 
 struct Node {
-
     int data;
-    struct Node* next;
+    struct Node *next;
 
 };
 
-void create_node() {
 
-struct Node *s, *first, *last, *p;
-
-s=(struct Node *)malloc(sizeof(struct Node));
-
-// here we are creating the first node
-
-s->data = 10;
-s->next = NULL; // initially we are setting it to null
-
-first=s;
-last=s;   // first and last are now pointing to s
-
-
-
-for (int i=0; i<5; i++) {
-
-    s=(struct Node *)malloc(sizeof(struct Node));
-
-    s->data = i;
-    s->next = NULL; //up until now we have created new node
-
-    last->next=s; // now we are assign the new node to previous node
-    last=s;       // now last is pointing to the last node
-
-}
-
-p=first;
-
-while(p != NULL) {
-
-printf("[%d] --> ", p->data);
-
-p=p->next;
-}
-
-}
 
 int main() {
 
-create_node();
+struct Node *s, *first, *last, *t, *q, *p;
+int pos;
+
+s=(struct Node *)malloc(sizeof(struct Node));
+
+s->data=10;
+s->next=NULL;
+
+first=s;
+last=s;
+
+for (int i=1; i<4; i++) {
+
+   s=(struct Node *)malloc(sizeof(struct Node));
+   s->data = i;
+   s->next = NULL;
+
+   last->next=s;
+   last=s;
+
+}
+
+
+// this is the position that we want to go upto
+t=first;
+
+for (int i = 0; i<2; i++) {
+
+    t=t->next;
+}
+
+s=(struct Node *)malloc(sizeof(struct Node));
+
+s->data=50;
+s->next=NULL; // upto this new node is ceated
+
+
+q=t->next;  // 'f'l't'      't'       't->next'
+t->next=s; //   |||          |         |
+s->next=q;//    [1]------->[2]------->[3]-------->[4]->NULL
+
+           //                [newnode]
+p=first;
+
+while(p!=NULL)
+{
+
+printf("\n %d ",p->data);
+
+p=p->next;
+}
 
 return 0;
 }
